a9630692084b6f60709356c4f11744796466d2de
siyu1915 <siyu1915@gmail.com>
2017-02-09 11:39:44 +0800

    Update installer

--- installer/jetbrains_installer.py
+++ installer/jetbrains_installer.py
@@ -8,16 +8,44 @@ def extract(tar_path, extract_path="."):
     tar.close()
 
 
+def for_extract(tar_files1, extract_path1):
+    for tar_file1 in tar_files1:
+        print("\t" + tar_file1)
+        extract(tar_file1, extract_path1)
+
+
 try:
     print("Welcome to Jetbrains Installer!")
     print("By siyu1915, https://www.github.com/siyu1915/")
+
     tar_files = glob.glob("./*.tar.gz")
     print("Input your extract path, enter . to extract current dir:")
     extract_path = input()
-    print("Extract path: " + extract_path)
-    print("Found these files(.tar.gz) in your directory:")
+    file_count = len(tar_files)
+    extracting_count = 1
+    print("Found " + str(file_count) + " files(.tar.gz) in your directory:")
+
+    if file_count == 0:
+        quit()
+
     for tar_file in tar_files:
         print("\t" + tar_file)
-        extract(tar_file, extract_path)
+
+    while True:
+        print("Are you sure you want to extract these files?[Y/n]")
+        i = input()
+        if i == "Y" or "y":
+            for_extract(tar_files, extract_path)
+            print("------------------------------")
+            print("Extracted " + str(file_count) + " file(s) to " + extract_path)
+            break
+        elif i == "N" or "n":
+            print("Exited.")
+            break
+        else:
+            continue
+
+
+
 except tarfile.TarError:
     print("Cannot extract files!")
